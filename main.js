d79d56a4b1e3889f1e4e1a389ad414656829de14
David Nolen <dnolen@cognitect.com>
2014-07-23 12:14:10 -0400

    wip

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
-var transit  = require("transit-js"),
+var express  = require("express"),
+    app      = express(),
+    transit  = require("transit-js"),
+    URI      = require("URIjs"),
+    fs       = require("fs"),
     handlers = require("./handlers");
 
 var r = transit.reader("json", {"handlers": handlers.readHandlers});
 var w = transit.writer("json", {"handlers": handlers.writeHandlers});
-var data = r.read('{"~#r":"https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi"}');
+var uri = URI("https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi");
 
-console.log(w.write(data));
+app.get("/", function(req, res) {
+    fs.readFile("index.html", "utf-8", function(err, data) {
+        if(!err) {
+            res.send(200, data);
+        } else {
+            res.send(404, "Page Not Found");
+        }
+    });
+});
+
+app.listen(3000);
+
