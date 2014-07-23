2f5e9b217502d3bc61ef44d24f6bdd329886a8da
David Nolen <dnolen@cognitect.com>
2014-07-23 15:50:46 -0400

    cleanup

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ var express  = require("express"),
     jsonRes  = require("./fake_api").JSON_RESULTS,
     transitRes = require("./fake_api").TRANSIT_RESULTS;
 
-var r = transit.reader("json", {"handlers": handlers.readHandlers});
 var w = transit.writer("json", {"handlers": handlers.writeHandlers});
-var uri = URI("https://api.twitter.com/1.1/search/tweets.json?q=%40twitterapi");
 
 app.use(express.static(__dirname, + "/bower_components"));
 app.use(express.static(__dirname, + "/shared"));
