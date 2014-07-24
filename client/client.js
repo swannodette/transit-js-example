077e6cbb924387284e58988266b251eb89e714c7
David Nolen <dnolen@cognitect.com>
2014-07-24 13:06:13 -0400

    tweak example to match blog post

--- client/client.js
+++ client/client.js
@@ -4,33 +4,43 @@
         h   = g.transitHandlers,
         rdr = g.transit.reader("json", {handlers: h.readHandlers});
 
+    // Reading transit, values are already the types your team expects
+    // just start programming
+
+    function parseTransitStatus(status) {
+        var user = status.get("user");
+        return {
+            weekLater: status.get("created_at").add("days", 7),
+            color: user.get("profile_sidebar_fill_color").cmyk(),
+            url: user.get("profile_image_url").path()
+        };
+    };
+
     j.ajax({
         type: "GET",
         url: "transit",
         complete: function(res) {
-            var data    = rdr.read(res.responseText),
-                status  = data.get("statuses")[0],
-                created = status.get("created_at"),
-                user    = status.get("user"),
-                rgb     = user.get("profile_sidebar_fill_color"),
-                url     = user.get("profile_image_url");
-
-            console.log(created.add("days", 7).format('MMMM Do YYYY, h:mm:ss a'));
-            console.log(rgb.cmyk());
-            console.log(url.protocol(), url.path());
+            var data = rdr.read(res.responseText),
+                xs   = data.get("statuses").map(parseTransitStatus);
+            console.log(xs);
         }
     });
-    
-    j.get("json", function(data) {
-        var status  = data["statuses"][0],
-            created = status["created_at"]
-            user    = status["user"],
-            rgb     = user["profile_sidebar_fill_color"],
-            url     = user["profile_image_url"];
 
-        console.log(created);
-        console.log(rgb);
-        console.log(url);
+    // Reading JSON, you always need to remember exactly what to hydrate
+    // to which types your team has agreed upon
+
+    function parseJSONStatus(status) {
+        var user = status["user"];
+        return {
+            weekLater: new Date(status["created_at"]).add("days", 7), // OOPS!!!
+            color: one.color(user["profile_sidebar_fill_color"]).cmyk(),
+            url: URI(user["profile_image_url"]).path() 
+        };
+    }
+
+    j.get("json", function(data) {
+        var xs = data["statuses"].map(parseJSONStatus);
+        console.log(xs);
     });
 
 })(this);
